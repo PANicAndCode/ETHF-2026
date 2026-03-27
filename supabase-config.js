7ff3159444915a0894119e32a1209cba9a02a1d0
PANicAndCode <totallynotascamboi@gmail.com>
2026-03-27 18:15:33 -0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "sb_publishable_kIAzbi6Oelk3FBuaU6wk9Q_yocdW7zQ",
+  url: "https://nbirlroxsiznvdnbynqv.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5iaXJscm94c2l6bnZkbmJ5bnF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2NDkxMTYsImV4cCI6MjA5MDIyNTExNn0.iM5ZO-wfQ9upvKKYXZxd5Bm7vV1gO193x-HJ1S68av8"
 };
